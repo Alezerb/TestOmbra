c32e2935f074cc5d75a345ede78782dbdf2a3421
Leonardo Martini (Laptop) <leonard00martini@gmail.com>
2020-12-10 16:15:02 +0100

    2019v Import Fresh Start

---
+++ Assets/Scenes/Harry Ari Scripts/CustomImageEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomImageEffect : MonoBehaviour
+{
+
+    public Material material;
+
+    public void Start() => Camera.main.depthTextureMode = DepthTextureMode.DepthNormals;// depthnormals are needed for outline effect
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, material);
+    }
+}
